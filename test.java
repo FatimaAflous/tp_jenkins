0637cd5f45e1bedabeae68100d2e82e9c3d1a7d0
Fatima Aflous <aflousfatima@gmail.com>
2024-03-26 11:55:36 +0000

    first commit

---
+++ test.java
@@ -0,0 +1,9 @@
+public class AfficherMot {
+    public static void main(String[] args) {
+        // Définition du mot à afficher
+        String mot = "Bonjour";
+
+        // Affichage du mot
+        System.out.println(mot);
+    }
+}
